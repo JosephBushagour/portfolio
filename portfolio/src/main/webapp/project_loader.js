1a75280863d994827ad7cdea991836dcd5b18844
Joey Bushagour <jbushagour@google.com>
2020-05-28 13:25:00 -0500

    Replace " with ' in js.

--- portfolio/src/main/webapp/project_loader.js
+++ portfolio/src/main/webapp/project_loader.js
@@ -15,29 +15,29 @@
 // The projects stored as an array of objects, later turned into html
 const projects = [
   {
-    title: "DIY Digital Watch",
-    image: "/images/watch.png",
-    link: "http://555dreams.blogspot.com/2018/10/creating-dip-attiny85-watch-with-ds3231.html",
-    description: "A digital watch on a custom PCB. Utilizes the AtTiny85 \
+    title: 'DIY Digital Watch',
+    image: '/images/watch.png',
+    link: 'http://555dreams.blogspot.com/2018/10/creating-dip-attiny85-watch-with-ds3231.html',
+    description: 'A digital watch on a custom PCB. Utilizes the AtTiny85 \
         microcontroller and DS3231 real-time-clock, as well as a 74HC595 shift \
-        register.",
-    keywords: ["Hardware", "Digital"],
+        register.',
+    keywords: ['Hardware', 'Digital'],
   },
   {
-    title: "Internet-connect Clock/Weather Station",
-    image: "/images/clock.png",
-    link: "http://555dreams.blogspot.com/2020/04/making-esp8266-based-clock-and-weather.html",
-    description: "A digital clock and weather station based off the ESP8266 \
-        microcontroller and the MAX7219 LED Drivers.",
-    keywords: ["Hardware", "Software", "Digital"],
+    title: 'Internet-connect Clock/Weather Station',
+    image: '/images/clock.png',
+    link: 'http://555dreams.blogspot.com/2020/04/making-esp8266-based-clock-and-weather.html',
+    description: 'A digital clock and weather station based off the ESP8266 \
+        microcontroller and the MAX7219 LED Drivers.',
+    keywords: ['Hardware', 'Software', 'Digital'],
   },
   {
-    title: "555-Timer Servo Driver",
-    image: "/images/servo.jpg",
-    link: "http://555dreams.blogspot.com/2018/09/driving-hc-sr04-ultrasonic-sensor-and.html",
-    description: "A servo driver created only using 555-timers, transistors, \
-     and analog components.",
-    keywords: ["Hardware", "Analog"],
+    title: '555-Timer Servo Driver',
+    image: '/images/servo.jpg',
+    link: 'http://555dreams.blogspot.com/2018/09/driving-hc-sr04-ultrasonic-sensor-and.html',
+    description: 'A servo driver created only using 555-timers, transistors, \
+     and analog components.',
+    keywords: ['Hardware', 'Analog'],
   },
 ]
 
@@ -57,7 +57,7 @@ function generateCard(project) {
             <p>${project.description}</p>
           </div>
           <div class="keywords">
-            <p>Tags: ${project.keywords.join(", ")}</p>
+            <p>Tags: ${project.keywords.join(', ')}</p>
           </div>
         </div>
       </div>
@@ -66,5 +66,5 @@ function generateCard(project) {
 }
 
 // Fills the cards div with each generated card
-document.getElementById("cards").innerHTML = 
-    projects.map(generateCard).join("");
+document.getElementById('cards').innerHTML = 
+    projects.map(generateCard).join('');
