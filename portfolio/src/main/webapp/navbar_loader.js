32a6e005d3d0e2d5b8f5130cb26fb02df031eb5a
Joey Bushagour <jbushagour@google.com>
2020-06-08 14:00:59 +0000

    Add navbar to index page through script.

---
+++ portfolio/src/main/webapp/navbar_loader.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const navbarHTML = `      
+    <a href="index.html">Home</a>
+    <a href="projects.html">Projects</a>
+    <a href="gardening.html">Gardening</a>
+    <a href="electronics.html">Electronics</a>
+    <a href="contact.html">Contact</a>
+    `;
+
+document.getElementById("navbar").innerHTML = navbarHTML;
