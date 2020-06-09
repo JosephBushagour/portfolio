958e73b7086a6ea3b7be0bbed653eb29b6277341
Joey Bushagour <jbushagour@google.com>
2020-06-09 20:11:44 +0000

    Reorder code for readability.

--- portfolio/src/main/webapp/charts.js
+++ portfolio/src/main/webapp/charts.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawPoll);
+
 const polls = {
   catsDogs: {
     title: 'Cats or Dogs',
@@ -32,11 +35,7 @@ function getPollOptions(polls) {
   `
   }).join('');
 }
-
 document.getElementById('polls-choice').innerHTML = getPollOptions(polls);
-
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawPoll);
 
 /**
  * Create the input form for user voting.
